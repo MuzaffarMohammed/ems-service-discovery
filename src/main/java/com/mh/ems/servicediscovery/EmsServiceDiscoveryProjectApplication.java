bcd3bd01c898712f5f72fe0b5508701df7fc0673
Muzaffar Mohammed <50576677+MuzaffarMohammed@users.noreply.github.com>
2020-11-06 18:39:09 +0530

    New service discovery server added

---
+++ src/main/java/com/mh/ems/servicediscovery/EmsServiceDiscoveryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mh.ems.servicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsServiceDiscoveryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsServiceDiscoveryProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mh/ems/servicediscovery/EmsServiceDiscoveryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mh.ems.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsServiceDiscoveryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
